436ff693a746ba7a995bf9e3ecf1d4d2b0da97bc
chenzhiy2001 <30455060+chenzhiy2001@users.noreply.github.com>
2025-10-21 06:01:13 +0200

    finish branch_and_bound implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from shutil import copy
 # scipy for MILP
 from scipy.optimize import linprog
 
@@ -147,9 +148,9 @@ def get_lower_bound_by_greedy(partial_schedule, current_timeslot, jobs):
     # compute reward for already FINISHED jobs in partial_schedule. Jobs that are halfway done or not yet started are being handled in the next part, and those that are not scheduled at all will be handled at the final part of this function.
     completed_jobs_reward = 0
     for job_instance in jobs.job_instances:
-        if list(partial_schedule.values()).count(job_instance.id) == job_instance.processing_time:
+        if list(partial_schedule["content"].values()).count(job_instance.id) == job_instance.processing_time:
             # job is finished, check if it's on time or tardy
-            completion_time = max(t for t, job_id in partial_schedule.items() if job_id == job_instance.id)
+            completion_time = max(t for t, job_id in partial_schedule["content"].items() if job_id == job_instance.id)
             if completion_time <= job_instance.deadline:
                 # on time
                 completed_jobs_reward += job_instance.reward
@@ -158,7 +159,7 @@ def get_lower_bound_by_greedy(partial_schedule, current_timeslot, jobs):
                 tardiness = completion_time - job_instance.deadline
                 tardiness_penalty = job_instance.penalty_function.evaluate(tardiness)
                 completed_jobs_reward += job_instance.reward - tardiness_penalty
-        elif list(partial_schedule.values()).count(job_instance.id) > job_instance.processing_time:
+        elif list(partial_schedule["content"].values()).count(job_instance.id) > job_instance.processing_time:
             # job is over-finished (should not happen in a valid schedule)
             raise ValueError(f"Job {job_instance.id} is over-finished.")
 
@@ -170,17 +171,17 @@ def get_lower_bound_by_greedy(partial_schedule, current_timeslot, jobs):
         # Get all jobs that can be scheduled in t, i.e., released by (which means before or at) t and not yet finished
         available_jobs = [
             job for job in jobs.job_instances
-            if job.release_time <= t and list(partial_schedule.values()).count(job.id) < job.processing_time
+            if job.release_time <= t and list(partial_schedule["content"].values()).count(job.id) < job.processing_time
         ]
         if available_jobs:
             # Sort available jobs by their deadline (earliest deadline first)
             available_jobs.sort(key=lambda x: x.deadline)
             # Assign the job with the earliest deadline to the current time slot
             selected_job = available_jobs[0]
-            partial_schedule[t] = selected_job.id
+            partial_schedule["content"][t] = selected_job.id
             # UPDATE the REWARD CHANGE
             # Check if the job is completed at this time slot
-            if list(partial_schedule.values()).count(selected_job.id) == selected_job.processing_time:
+            if list(partial_schedule["content"].values()).count(selected_job.id) == selected_job.processing_time:
                 # Job is completed, check if it's on time or tardy
                 completion_time = t
                 if completion_time <= selected_job.deadline:
@@ -195,7 +196,7 @@ def get_lower_bound_by_greedy(partial_schedule, current_timeslot, jobs):
 
     # final part: compute penalty for unfinished or unscheduled jobs
     for job_instance in jobs.job_instances:
-        if list(partial_schedule.values()).count(job_instance.id) < job_instance.processing_time:
+        if list(partial_schedule["content"].values()).count(job_instance.id) < job_instance.processing_time:
             # Job is unfinished, apply penalty
             completed_jobs_reward -= job_instance.drop_penalty
 
@@ -375,7 +376,7 @@ def get_upper_bound_by_MILP(partial_schedule, current_timeslot, jobs):
                     bounds.append((0, 1))
                 else:
                     # for time slots before current time slot, x_i_t is the value in partial_schedule
-                    assigned_job_id = partial_schedule.get(t_0_based + 1, None)
+                    assigned_job_id = partial_schedule["content"].get(t_0_based + 1, None)
                     if assigned_job_id == index_to_job_id[job_index]:
                         bounds.append((1, 1))
                     else:
@@ -409,39 +410,107 @@ def schedule_jobs(jobs):
     - Then we will do branch-and-bound to find the optimal job assignment for current time slot.
     '''
     # we define partial_schedule as a dictionary mapping time slot to job id assigned at that time slot.
-    partial_schedule = {}
-    # the scheduler schedules jobs from time slot 1 to time slot total_time_slots.
-    for current_timeslot in range(1, jobs.total_time_slots + 1):
-        # assignment_bounds is an object mapping job id to (lower_bound, upper_bound)
-        assignment_bounds = {}
+    root_partial_schedule = {
+        "id": 0,
+        "content": {}, 
+        "lower_bound": float('-inf'),
+        "upper_bound": float('inf'),
+        "parent_id": None,
+        "children_ids": []
+    }
+    # since there are frequent backtracks to update ancestor nodes' bounds, we will maintain the branch-and-bound tree instead of just a queue of partial schedules.
+    branch_and_bound_tree = {}
+    # the way to add a new partial schedule to the tree
+    branch_and_bound_tree[root_partial_schedule["id"]] = root_partial_schedule
+
+    # we start expanding the tree starting from time slot 1
+    current_timeslot = 1
+    # start from the root node
+    BFS_queue = [branch_and_bound_tree[0]]
+    deleted_nodes = []
+    # solutions where lower bound == upper bound
+    optimal_solutions = [] # todo
+    while BFS_queue:
+        partial_schedule = BFS_queue.pop(0)
+        # examine all "child nodes" (partial schedules) at the current time slot
         for job_instance in jobs.job_instances:
-            # if the job is already finished, skip it. Be careful that one value in partial_schedule corresponds to one time slot, so job_instance.id in partial_schedule.values() does not necessarily mean the job is finished.
-            if list(partial_schedule.values()).count(job_instance.id) == job_instance.processing_time:
+            # start by ditching infeasible children nodes
+            # if the job is already finished, skip it. Be careful that one value in partial_schedule corresponds to one time slot, so job_instance.id in partial_schedule["content"].values() does not necessarily mean the job is finished.
+            if list(partial_schedule["content"].values()).count(job_instance.id) == job_instance.processing_time:
                 continue
-            elif list(partial_schedule.values()).count(job_instance.id) > job_instance.processing_time:
+            elif list(partial_schedule["content"].values()).count(job_instance.id) > job_instance.processing_time:
                 raise ValueError(f"Job {job_instance.id} has been assigned more time slots than its processing time.")
             # if the job is not yet released, skip it
             if current_timeslot < job_instance.release_time:
                 continue
-            # otherwise, assume we assign this job to the current time slot
-            partial_schedule_assumption = partial_schedule.copy()
-            partial_schedule_assumption[current_timeslot] = job_instance.id
+            # Now we are sure that this job assignment at this timeslot is feasible, assume we assign this job to the current time slot
+            # use object DEEP COPY to avoid modifying the original partial_schedule
+            partial_schedule_assumption = copy.deepcopy(partial_schedule)
+            partial_schedule_assumption["content"][current_timeslot] = job_instance.id
             current_timeslot_assumption = current_timeslot + 1
-            lower_bound_for_this_assumption = get_lower_bound_by_greedy(partial_schedule_assumption, current_timeslot_assumption, jobs)
-            upper_bound_for_this_assumption = get_upper_bound_by_MILP(partial_schedule_assumption, current_timeslot_assumption, jobs)
-            assignment_bounds[job_instance.id] = (lower_bound_for_this_assumption, upper_bound_for_this_assumption)
-        # do branch-and-bound to find the optimal job assignment for current time slot
-        
-
-        
-        # deal with the last time slot will be tricky because there is no next time slot to compute bounds for, so we just pick the job with the highest lower bound
-
-
-    # partial_schedule now contains the job assignments for all time slots
-    return partial_schedule
-
-
-
+            if current_timeslot < jobs.total_time_slots:
+                lower_bound_for_this_assumption = get_lower_bound_by_greedy(partial_schedule_assumption, current_timeslot_assumption, jobs)
+                upper_bound_for_this_assumption = get_upper_bound_by_MILP(partial_schedule_assumption, current_timeslot_assumption, jobs)
+                # first we add the new schedule to branch_and_bound_tree
+                new_partial_schedule_id = len(branch_and_bound_tree)
+                new_partial_schedule = {
+                    "id": new_partial_schedule_id,
+                    "content": partial_schedule_assumption["content"], 
+                    "lower_bound": lower_bound_for_this_assumption,
+                    "upper_bound": upper_bound_for_this_assumption,
+                    "parent_id": partial_schedule["id"],
+                    "children_ids": []
+                }
+                branch_and_bound_tree[new_partial_schedule_id] = new_partial_schedule
+                branch_and_bound_tree[partial_schedule["id"]]["children_ids"].append(new_partial_schedule_id)
+                # then we decide whether to add this new schedule to BFS queue for FURTHER expansion
+                if lower_bound_for_this_assumption < upper_bound_for_this_assumption:  # add to BFS queue for further expansion
+                    BFS_queue.append(new_partial_schedule)
+                elif lower_bound_for_this_assumption == upper_bound_for_this_assumption:  # found optimal solution for this branch, can stop expanding further
+                    pass
+                else:
+                    raise ValueError("Lower bound exceeds upper bound, which should not happen.")
+            elif current_timeslot == jobs.total_time_slots:  # last time slot, no need to compute bounds for next time slot
+                # calculate the exact reward for this complete schedule
+                final_reward = get_lower_bound_by_greedy(partial_schedule_assumption, current_timeslot_assumption, jobs) # in this case, current_timeslot_assumption = total_time_slots + 1, so it exceeds total time slots
+                # we add the new schedule to branch_and_bound_tree
+                new_partial_schedule_id = len(branch_and_bound_tree)
+                new_partial_schedule = {
+                    "id": new_partial_schedule_id,
+                    "content": partial_schedule_assumption["content"], 
+                    "lower_bound": final_reward,
+                    "upper_bound": final_reward,
+                    "parent_id": partial_schedule["id"],
+                    "children_ids": []
+                }
+                branch_and_bound_tree[new_partial_schedule_id] = new_partial_schedule
+                branch_and_bound_tree[partial_schedule["id"]]["children_ids"].append(new_partial_schedule_id)
+                # no need to add to BFS queue since this is the last time slot
+            else:
+                raise ValueError("Current time slot exceeds total time slots, which should not happen.")
+        # after examining all child nodes (aka all possible, feasible job assignments) at the current time slot, we need to backtrack to update ancestor nodes' bounds
+        # we do this by traversing up the tree from the current partial_schedule to the root
+        update_range_node = partial_schedule
+        while True:
+            # update parent's lower bound and upper bound
+            child_lower_bounds = [branch_and_bound_tree[child_id]["lower_bound"] for child_id in update_range_node["children_ids"]]
+            child_upper_bounds = [branch_and_bound_tree[child_id]["upper_bound"] for child_id in update_range_node["children_ids"]]
+            # new lower bound is the MAXIMUM of children's lower bounds and parent's lower bound
+            update_range_node["lower_bound"] = max(child_lower_bounds + [update_range_node["lower_bound"]])
+            # new upper bound is the biggest amongst those that are >= parent's new lower bound, NOT including parent's old upper bound
+            update_range_node["upper_bound"] = max(ub for ub in child_upper_bounds if ub >= update_range_node["lower_bound"])
+            # before moving up we need to delete some branches whose upper bound < parent's new lower bound
+            for child_id in update_range_node["children_ids"]:
+                if branch_and_bound_tree[child_id]["upper_bound"] < update_range_node["lower_bound"]:
+                    # remove this child id from parent's children_ids so it's not reachable in the tree anymore
+                    update_range_node["children_ids"].remove(child_id)
+                    # add this child id to deleted_nodes for record
+                    deleted_nodes.append(child_id)
+            # move up to parent
+            if update_range_node["parent_id"] is not None:
+                update_range_node = branch_and_bound_tree[update_range_node["parent_id"]]
+            else:
+                break
 
 def main():
     jobs = load_jobs_from_input_file("input.json")
